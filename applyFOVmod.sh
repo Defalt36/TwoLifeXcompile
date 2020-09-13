6210550230ea7a0999881360435b1ef7a30e961a
Defalt36 <FaelOtk@gmail.com>
2020-09-12 21:35:57 -0300

    Delete applyFOVmod.sh

--- applyFOVmod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../OneLife
-
-git remote add fovmod "https://github.com/Joriom/OneLifeXcompile.git"
-git fetch fovmod
-git cherry-pick 384e3c0284694edea1cd4e9c407ac19f543d6410
-git mergetool
