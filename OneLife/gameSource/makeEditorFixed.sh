a636ed861f4c19b8c5b2f1c9bceb397b23ab49ee
Defalt36 <faelotk@gmail.com>
2021-02-01 15:36:46 -0300

    makeEditorFixed does not depends on makeEditor

--- OneLife/gameSource/makeEditorFixed.sh
+++ OneLife/gameSource/makeEditorFixed.sh
@@ -12,7 +12,12 @@ cp Makefile Makefile.bak;
 sed -i '/^GXX =/s,$, -static `sdl-config --static-libs`,' Makefile;
 sed -i -r '/^PLATFORM_LIBPNG_FLAG =/aPLATFORM_LIBPNG_FLAG = -lpng -lz' Makefile;
 
-./makeEditor.sh;
+mv makeFileList makeFileList.bak
+cp makeFileListEditor makeFileList
+
+make
+
+mv makeFileList.bak makeFileList
 
 export PATH="${_OLD_PATH}"; unset _OLD_PATH;
 mv Makefile.bak Makefile;
